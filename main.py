01885359648dd0a076ee5b254455d32bbf8e7fbf
Dima457 <144612480+Dima457@users.noreply.github.com>
2023-09-10 18:57:51 +0300

    Update main.py
    пытаюсь сделать так чтобы функция отключения мата работала

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_text_messages(message):
             # 'привет', 'hi', 'hello')
             if message.text.lower().find(j) != -1: # если содержимое переменной есть в сообщении (например 'как дела' в 'как дела?')
                 bot.send_message(message.chat.id, f'{random.choice(phrases.xx(i))}') # то отправляем сообщение с рандомным текстом из переменной для ответа
-            elif  message.text.lower() in mat:
+            elif  message.text.lower().find(j) in mat:
                 bot.delete_message(message.chat.id, message.message_id) 
                 # !!!важно!!! названия перемнных ответа должны быть вида название_переменной_otv
                 flag = False # если содержимое переменной есть в сообщении то меняем значение флага, тем самым сигнализируем, что сообщение отнесенно к какой либо категории (есть в заранее добавленных вариантах ответ)
