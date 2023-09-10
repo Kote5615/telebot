8dfbc938ea0013aaba0be753e13e7261b8649845
Kote5615 <82301930+Kote5615@users.noreply.github.com>
2023-09-10 20:22:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 import random
 import phrases
+
 bot = telebot.TeleBot('6379345549:AAEN-otxDH-btN0XesngJgVesO1TivJsYss')
 
 
@@ -8,20 +9,27 @@ bot = telebot.TeleBot('6379345549:AAEN-otxDH-btN0XesngJgVesO1TivJsYss')
 def start(message):
     bot.send_message(message.chat.id, 'Привет,смотри что я могу.По умолчанию я в режиме обычного диалога но есть и '
                                       'спец режимы Скажи: /shytka- расскажу шутку, и так каждый раз когда будешь писать это слово')
+
+
 @bot.message_handler(commands=['shytka'])
 def shytka(message):
-       bot.send_message(message.chat.id, random.sample(phrases.jokes, 1))
+    bot.send_message(message.chat.id, random.sample(phrases.jokes, 1))
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    flag = True
-    for i in phrases.recieve_nicknames:
-        for j in phrases.recieve[phrases.recieve_nicknames.index(i)]:
-            if message.text.lower().find(j) != -1:
-                bot.send_message(message.chat.id, f'{random.choice(phrases.xx(i))}')
-                flag = False
-                break
-    if flag is True:
-        bot.send_message(message.chat.id, random.sample(phrases.general, 1))
+    flag = True  # устанавливаем флаг
+    for i in phrases.recieve_nicknames:  # проходимся по всем возможным названиям переменных которые у тебя есть (переменные 'hi', 'how_you_name', 'how_are_you', 'insults',
+        # 'ot_how_are_you', 'what_do_you', 'good_night', 'good_morning')
+        for j in phrases.recieve[phrases.recieve_nicknames.index(i)]:  # для каждого названия переменной проверяем содержимое переменной ( к примеру для hi это будут 'приветик',
+            # 'привет', 'hi', 'hello')
+            if message.text.lower().find(j) != -1: # если содержимое переменной есть в сообщении (например 'как дела' в 'как дела?')
+                bot.send_message(message.chat.id, f'{random.choice(phrases.xx(i))}') # то отправляем сообщение с рандомным текстом из переменной для ответа
+                # !!!важно!!! названия перемнных ответа должны быть вида название_переменной_otv
+                flag = False # если содержимое переменной есть в сообщении то меняем значение флага, тем самым сигнализируем, что сообщение отнесенно к какой либо категории (есть в заранее добавленных вариантах ответ)
+                break #заканчиваем цикл в случае успеха
+    if flag is True: # если цикл не нашел преднаписанный сценарий
+        bot.send_message(message.chat.id, random.sample(phrases.general, 1)) #то отправляем рандомный смайл из переменной general
+
 
 bot.polling(none_stop=True)
